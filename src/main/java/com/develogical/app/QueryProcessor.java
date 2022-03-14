859aa1061e190788c1a9cf0d7c28d0bbf161e533
MaxMargalith <max_andreas@web.de>
2022-03-14 16:48:48 +0000

    Introduced intentional bug

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -3,7 +3,7 @@ package com.develogical.app;
 public class QueryProcessor {
     public String process(String query) {
         // This is where the magic happens
-        if (query.toLowerCase().contains("shakespeare")) {
+        if (query.toLowerCase().contains("shakespear")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
