3080b8ecb17be924d8abc3dbb52a9f6fa4e5d663
MaxMargalith <max_andreas@web.de>
2022-03-17 10:38:34 +0000

    added curie support

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -11,6 +11,9 @@ public class QueryProcessor {
         else if (query.toLowerCase().contains("putin")) {
             return "That guy's a massive dickhead.";
         }
+        else if (query.toLowerCase().contains("marie curie")) {
+            return "Marie Salomea Skłodowska Curie (/ˈkjʊəri/ KURE-ee,[3] pronounced [ˈmarja skwɔˈdɔfska kʲiˈri] (audio speaker iconlisten); born Maria Salomea Skłodowska, Polish: [ˈmarja salɔˈmɛa skwɔˈdɔfska]; 7 November 1867 – 4 July 1934) was a Polish and naturalized-French physicist and chemist who conducted pioneering research on radioactivity.";
+        }
         return "";
     }
 }
