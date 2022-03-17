e3d367060fca93f36c2c7ee5a4362a10b0383518
MaxMargalith <max_andreas@web.de>
2022-03-17 10:50:01 +0000

    added multiple curie support

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,4 +5,7 @@ node {
    stage('Build') {
       sh "./gradlew clean test"
    }
+   stage('Deploy') {
+      sh "git push https://git.heroku.com/evening-shelf-79025.git master"
+   }
 }
--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -14,6 +14,9 @@ public class QueryProcessor {
         else if (query.toLowerCase().contains("marie curie")) {
             return "Marie Salomea Skłodowska Curie (/ˈkjʊəri/ KURE-ee,[3] pronounced [ˈmarja skwɔˈdɔfska kʲiˈri] (audio speaker iconlisten); born Maria Salomea Skłodowska, Polish: [ˈmarja salɔˈmɛa skwɔˈdɔfska]; 7 November 1867 – 4 July 1934) was a Polish and naturalized-French physicist and chemist who conducted pioneering research on radioactivity.";
         }
+        else if (query.toLowerCase().contains("pierre curie")) {
+            return "Pierre Curie (/ˈkjʊəri/ KURE-ee,[1] French: [pjɛʁ kyʁi]; 15 May 1859 – 19 April 1906) was a French physicist, a pioneer in crystallography, magnetism, piezoelectricity, and radioactivity. In 1903, he received the Nobel Prize in Physics with his wife, Marie Curie, and Henri Becquerel, in recognition of the extraordinary services they have rendered by their joint researches on the radiation phenomena discovered by Professor Henri Becquerel.";
+        }
         return "";
     }
 }
